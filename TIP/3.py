d721cbdce32d0e03158196a0627bae12c2ba728e
LEE HYE JIN <108391677+Hiluxy@users.noreply.github.com>
2022-09-28 10:13:20 +0900

    0928

--- TIP/3.py
+++
@@ -1,6 +0,0 @@
-    
-def solution(commands): 
-    table = [['O']*51] * 51
-
-c1=["UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"]
-print(solution(c1))
