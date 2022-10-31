a9a59ef5da36113c722d17ff90662352b6465b23
Hiluxy <sunset.lhj@gmail.com>
2022-10-31 20:10:00 +0900

    1031

--- 재귀/로또.py
+++ 재귀/로또.py
@@ -1,15 +1,17 @@
+#재귀,문자열,조합
+
 def comb(arr, n):
     result = []
-    if n > len(arr):
-        return result
+    # if n > len(arr):
+    #     return result
  
     if n == 1:
         for i in arr:
             result.append([i])
            
     elif n > 1:
-        for i in range(len(arr) - n + 1):
-            for j in comb(arr[i + 1:], n - 1):
+        for i in range(len(arr) - n + 1): #[1,2,3,4,5,6,7] 이면 i=0,1
+            for j in comb(arr[i + 1:], n - 1): #i=0일때 0다음부터 조합 찾기
                 result.append([arr[i]] + j)
  
     return result
@@ -27,9 +29,3 @@ if __name__ == "__main__":
                 print(' '.join(c))
             print()
 
-    # arr=list(map(int,input().split()))
-    # n=arr.pop()
-    # # for com in comb(arr,n):
-    # #     print(com)
-    # result=comb([1,2,3,5,8,13,21,34],6)
-    # print
