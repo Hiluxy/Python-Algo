fe1366619e57c2f24001c01966824e8dc3ec797e
LEE HYE JIN <108391677+Hiluxy@users.noreply.github.com>
2023-05-10 21:40:07 +0900

    second

--- 투포인터/두 개의 탑.py
+++ 투포인터/두 개의 탑.py
@@ -1,17 +1,27 @@
+import sys
+input=sys.stdin.readline
 n=int(input())
 arr=[int(input()) for _ in range(n)]
 
-start=0
-result=0
-for start in range(n):
-    distance=0
-    for point in range(start+1,n):
-        distance+=arr[point]
-        if distance<=sum(arr)-distance: 
-            result=max(result,distance)
-        else:
-            continue
+total=sum(arr)
 
+left=right=dist=result=0
+i=0
+while 1:
+    if dist>result:
+        dist-=arr[left]
+        left+=1 
+    if right==n:
+        break
+    else:
+        dist+=arr[right]
+        right+=1
+    
+    if dist==total//2:
+        result=dist
+        break
+    #결과 갱신
+    result=max(result, min(dist,total-dist))
+    print(i,left,right)
+    i+=1
 print(result)
-        
-        
